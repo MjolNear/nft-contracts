f062c3b8fc04ea941a66b80496a74f04446af4b4
Oleg Bobrov <njancrafttv@gmail.com>
2021-12-14 21:18:36 +0300

    - add rm old "out" in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+rm -r out
 RUSTFLAGS='-C link-arg=-s' cargo +stable build --target wasm32-unknown-unknown --release
 mkdir out
 cp target/wasm32-unknown-unknown/release/*.wasm ./out/main.wasm
